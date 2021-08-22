73015dac1399370de130b499fc2df12141440b0b
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-08-22 21:17:19 +0530

    Added files

---
+++ Learning/Essential-Concepts/1-Array-Basics.cpp
@@ -0,0 +1,18 @@
+/**
+ * Arrays are collection of similar data elements
+ * int A[5] declares an array of integers of size 5
+ * Main Memory is divided into three parts: 
+ * ? Stack, Heap and Code Section
+ * Created array will be in Stack and directly accessible to the main function
+ * 
+ *  
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[5];                    //declaration
+    int B[5] = {1, 2, 3, 4, 5};  //declaration with initialistion
+    // A[0] to access first element
+    return 0;
+}
