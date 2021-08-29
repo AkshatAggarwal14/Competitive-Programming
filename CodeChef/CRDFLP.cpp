84732feb5a6181482e2b2881e755e9c457087f4c
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-08-29 19:44:41 +0530

    Upsolved

--- CodeChef/CRDFLP.cpp
+++ CodeChef/CRDFLP.cpp
@@ -91,153 +91,44 @@ void print(T &&t, Args &&...args) {
     print(forward<Args>(args)...);
 }
 
-void Solution() {
+void solve() {
     ll n;
     cin >> n;
     vl a(n), b(n);
-    vector<bool> mark1(n, false), mark2(n, false);
-    cin >> a;
-    cin >> b;
-    //start with top card
+    cin >> a >> b;
+    // 2^30
+    //we can go from MSB to LSB and check which can be set to maximise
+    //for each card- undecided, flipped, not flipped
+    // -1 , 0 , 1
 
-    fo(i, n) {
-        if (a[i] == 0 && b[i] == 0) {
-            print(0, 0);
-            return;
+    //kth bit set or not set
+    vl state(n, -1);
+    for (ll bit = 1LL << 30; bit > 0; bit >>= 1) {
+        bool pos = true;  //assume bit is set
+        fo(i, n) {
+            if (state[i] == 0 and !(a[i] & bit))
+                pos = false;
+            else if (state[i] == 1 and !(b[i] & bit))
+                pos = false;
+            else if (!(a[i] & bit) and !(b[i] & bit))
+                pos = false;
+        }
+        if (!pos) continue;
+        fo(i, n) {
+            if (state[i] != -1) continue;
+            if (!(a[i] & bit)) state[i] = 1;
+            if (!(b[i] & bit)) state[i] = 0;
         }
     }
-
-    bool flag1 = false;
-
-    ll res1 = a[0];
-    rep(i, 1, n) {
-        ll val1 = (res1 & a[i]), val2 = (res1 & b[i]);
-        if (val1 == 0 && val2 == 0) flag1 = true;
-        if (val1 >= val2) {
-            res1 = val1;
-        } else {
-            res1 = val2;
-            mark1[i] = true;
-        }
+    ll ans = (1LL << 30) - 1, flips = 0;
+    for (ll i = 0; i < n; i++) {
+        if (state[i] == 1) {
+            flips++;
+            ans &= b[i];
+        } else
+            ans &= a[i];
     }
-    ll cnt1 = count(all(mark1), true);
-
-    bool flag2 = false;
-    ll res2 = b[0];
-    mark2[0] = true;
-    rep(i, 1, n) {
-        ll val1 = (res2 & a[i]), val2 = (res2 & b[i]);
-        if (val1 == 0 && val2 == 0) flag2 = true;
-
-        if (val1 >= val2) {
-            res2 = val1;
-        } else {
-            res2 = val2;
-            mark2[i] = true;
-        }
-    }
-    ll cnt2 = count(all(mark2), true);
-
-    if (flag1 && flag2) {
-        print(0, 0);
-        return;
-    }
-
-    // print(res1, res2);
-    if (res1 > res2) {
-        print(res1, cnt1);
-    } else if (res2 > res1) {
-        print(res2, cnt2);
-    } else {
-        print(res1, min(cnt1, cnt2));
-    }
-}
-//*read stuff at the bottom
-
-void solve() {
-    ll n, ans = 0, mn = 0;
-    cin >> n;
-    vl a(n), b(n);
-    fo(i, n) cin >> a[i];
-    fo(i, n) cin >> b[i];
-    vl opposite(n, -1);
-    for (ll i = 30; i >= 0; i--) {
-        ll possi = 1;
-        fo(j, n) {
-            if (opposite[j] != -1) {
-                if (opposite[j] == 1)
-                    possi = ((possi & (b[j] >> i) & 1));
-                else
-                    possi = ((possi & (a[j] >> i) & 1));
-                continue;
-            }
-            if (((a[j] >> i) & 1) || ((b[j] >> i) & 1)) {
-            } else {
-                possi = 0;
-                break;
-            }
-        }
-        if (!possi)
-            continue;
-        ll val = 1;
-        fo(j, n) {
-            if (opposite[j] != -1) {
-                if (opposite[j] == 1)
-                    val = ((val & (b[j] >> i) & 1));
-                else
-                    val = ((val & (a[j] >> i) & 1));
-                continue;
-            }
-
-            if (((a[j] >> i) & 1) && ((b[j] >> i) & 1)) {
-            } else if (((a[j] >> i) & 1))
-                opposite[j] = 0;
-            else if (((b[j] >> i) & 1))
-                opposite[j] = 1, mn++;
-            else {
-                val = 0;
-                break;
-            }
-        }
-        ans += (val * (1LL << i));
-    }
-    print(ans, mn);
-    // ll n;
-    // cin >> n;
-    // vl a(n), b(n);
-    // cin >> a >> b;
-    // bool flag = false, flag1 = false, flag2 = false;
-    // ll res1 = a[0], res2 = b[0];
-    // if (res1 == 0 && res2 == 0) flag = true;
-    // ll cnt1 = 0, cnt2 = 1;
-    // rep(i, 1, n) {
-    //     ll v11 = (res1 & a[i]), v12 = (res1 & b[i]);
-    //     ll v21 = (res2 & a[i]), v22 = (res2 & b[i]);
-    //     if (v11 == 0 && v12 == 0) flag1 = true;
-    //     if (v21 == 0 && v22 == 0) flag2 = true;
-
-    //     if (v11 >= v12)
-    //         res1 = (res1 & v11);
-    //     else
-    //         res1 = (res1 & v12), cnt1++;
-
-    //     if (v21 >= v22)
-    //         res2 = (res2 & v21);
-    //     else
-    //         res2 = (res2 & v22), cnt2++;
-    // }
-
-    // ll ans = max(res1, res2);
-    // if ((flag) || (flag1 && flag2))
-    //     print(0, 0);
-    // else if (ans == 0)
-    //     print(0, 0);
-    // else if (ans == res1 && res1 == res2)
-    //     print(res1, min(cnt1, cnt2));
-    // else if (ans == res1)
-    //     print(res1, cnt1);
-    // else
-    //     print(res2, cnt2);
+    print(ans, flips);
 }
 
 int main() {
