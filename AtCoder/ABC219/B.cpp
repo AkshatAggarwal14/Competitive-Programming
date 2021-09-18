2537fd0e984eba53ac761a81634504cae7a59dbe
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-18 19:51:45 +0530

    Added ABC219

---
+++ AtCoder/ABC219/B.cpp
@@ -0,0 +1,62 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
+using ll = int64_t;
+using str = string;
+template <class T>
+using v = vector<T>;
+using vl = v<ll>;
+using vvl = v<vl>;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void Solution(ll _i) {
+    // cout << "Case #" << _i << ": ";
+    str s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    str t;
+    cin >> t;
+    fo(i, sz(t)) {
+        if (t[i] == '1')
+            cout << s1;
+        else if (t[i] == '2')
+            cout << s2;
+        if (t[i] == '3') cout << s3;
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cin.tie(nullptr)->sync_with_stdio(false);
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) Solution(i);
+    return 0;
+}
+
+/*
+    ? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * modulo of negative numbers is not a%b, it is a%b + abs(b)
+    * When using a set, lower_bound(all(set),l) is slower than set.lower_bound(l) because of random iterators
+    * string .append() or += is O1, but s = s + s is On (as it creates a copy first), use wisely
+    * DONT GET STUCK ON ONE APPROACH
+    * use __lg(n) instead of log2(n), int: 32 - __builtin_clz(n), ll: 63 - __builtin_clzll(n), https://codeforces.com/blog/entry/45966
+    * string.rfind() finds first occurence from end
+    * (a & b) + (a | b) = a + b
+    * TLE due to ll ? or multiple copies of large DS?
+*/
