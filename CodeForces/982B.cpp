ae038ae118e826163d3d3792d4ea60d06408a641
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-19 18:06:23 +0530

    Upsolved

---
+++ CodeForces/982B.cpp
@@ -0,0 +1,110 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using ll = int64_t;
+using db = double;
+using str = string;
+using ull = unsigned long long;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define set_bits(x) __builtin_popcountll(x)
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define uniq(x) (x).erase(unique(all(x)), (x).end())
+#define ps(x, y) fixed << setprecision(y) << x
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define sz(x) ((ll)(x).size())
+template <class T>
+using V = vector<T>;
+using vl = V<ll>;
+using vvl = V<vl>;
+template <class T, class U>
+using P = pair<T, U>;
+using pl = P<ll, ll>;
+using vpl = V<pl>;
+const ll mod = 1e9 + 7;  //1000000007
+const ll mod2 = 998244353;
+const ll inf = LLONG_MAX;
+const db eps = 1e-12;
+const double PI = acos(-1);
+
+template <class T, class U = T>
+bool amin(T& a, U&& b) {
+    return b < a ? a = std::forward<U>(b), true : false;
+}
+template <class T, class U = T>
+bool amax(T& a, U&& b) {
+    return a < b ? a = std::forward<U>(b), true : false;
+}
+
+void Solution(ll _i) {
+    // cout << "Case #" << _i << ": ";
+    ll n, num;
+    cin >> n;
+    set<pair<ll, ll>> All;
+    fo(i, n) cin >> num, All.insert({num, i + 1});
+    str s;
+    cin >> s;
+    set<pair<ll, ll>> Intro;
+    fo(i, sz(s)) {
+        if (s[i] == '0') {  //introvert chooses min width empty seat
+            auto it = All.begin();
+            cout << (*it).second << " ";
+            Intro.insert(*it);  //! insert before erasing
+            All.erase(*it);
+        } else {  //extrovert chooses max width introvert filled seat
+            // assert(Intro.empty() == false);
+            auto it = Intro.rbegin();
+            cout << (*it).second << " ";
+            Intro.erase(*it);
+        }
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cin.tie(nullptr)->sync_with_stdio(false);
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) Solution(i);
+    cerr << "\n";
+    cerr << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
+
+/*
+    ? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * modulo of negative numbers is not a%b, it is a%b + abs(b)
+    * When using a set, lower_bound(all(set),l) is slower than set.lower_bound(l) because of random iterators
+    * string .append() or += is O1, but s = s + s is On (as it creates a copy first), use wisely
+    * DONT GET STUCK ON ONE APPROACH
+    * use __lg(n) instead of log2(n), int: 32 - __builtin_clz(n), ll: 63 - __builtin_clzll(n), https://codeforces.com/blog/entry/45966
+    * string.rfind() finds first occurence from end
+    * (a & b) + (a | b) = a + b
+    * TLE due to ll ? or multiple copies of large DS?
+*/
