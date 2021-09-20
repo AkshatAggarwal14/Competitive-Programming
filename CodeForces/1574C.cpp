97e2e30ca8d1daf0e9f957e177bb3a8b1d4aaacb
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-21 00:10:42 +0530

    Added R1574

--- CodeForces/1574C.cpp
+++ CodeForces/1574C.cpp
@@ -94,7 +94,7 @@ void Solution() {
         // just check 2 options, nearest v[i] and nearest sum - v[i]
         ll coins{inf};
         if (i < n - 1)
-            coins = max(y - (sum - v[i]), 0LL) + max(x - (v[i]), 0LL);
+            amin(coins, max(y - (sum - v[i]), 0LL) + max(x - (v[i]), 0LL));
         if (i > 0)
             amin(coins, max(y - (sum - v[i - 1]), 0LL) + max(x - (v[i - 1]), 0LL));
         cout << coins << '\n';
