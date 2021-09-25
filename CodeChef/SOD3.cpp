d6ea810cb8248de88d9bd1fa0ce8e22c3a06cef9
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-25 21:42:07 +0530

    Added LTIME100B

---
+++ CodeChef/SOD3.cpp
@@ -0,0 +1,42 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
+using ll = int64_t;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define sz(x) ((ll)(x).size())
+const double eps = 1e-12;
+const double PI = acos(-1);
+
+void Solution() {
+    ll l, r;
+    cin >> l >> r;
+    if (l % 3 == 0)
+        cout << r / 3 - l / 3 + 1;
+    else
+        cout << r / 3 - l / 3;
+    cout << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#ifdef NCR
+    init();
+#endif
+    ll tc = 1;
+    cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        Solution();
+    }
+    return 0;
+}
