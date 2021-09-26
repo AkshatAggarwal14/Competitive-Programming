d844849a55c6fb1621b1722648c837b526424c50
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-27 00:11:29 +0530

    Upsolved

---
+++ AtCoder/ABC048/B.cpp
@@ -0,0 +1,41 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
+using ll = int64_t;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define sz(x) ((ll)(x).size())
+const double eps = 1e-12;
+const double PI = acos(-1);
+
+void Solution() {
+    ll a, b, x;
+    cin >> a >> b >> x;
+    if (a == 0)
+        cout << b / x + 1;
+    else
+        cout << b / x - (a - 1) / x;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#ifdef NCR
+    init();
+#endif
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        Solution();
+    }
+    return 0;
+}
