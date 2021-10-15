32859d909b5cf44ed41bca1dadc083c5a3090948
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-16 00:30:53 +0530

    dfs and floyd warshall

--- CSES/1672.cpp
+++ CSES/1672.cpp
@@ -1,9 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = int64_t;
-const ll inf = LLONG_MAX;
+const ll inf = 1e18;
 
+//! Floyd–Warshall algorithm
 int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll n, m, q, u, v, w, a, b;
+    cin >> n >> m >> q;
+    vector<vector<ll>> dist(n, vector<ll>(n, inf));
+    for (ll i = 0; i < m; ++i) {
+        cin >> u >> v >> w;
+        u--, v--;
+        dist[u][v] = min(w, dist[u][v]), dist[v][u] = min(w, dist[v][u]);
+    }
+
+    for (ll i = 0; i < n; i++) dist[i][i] = 0;
+
+    for (ll k = 0; k < n; k++) {
+        for (ll i = 0; i < n; i++) {
+            for (ll j = 0; j < n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    while (q--) {
+        cin >> a >> b;
+        a--, b--;
+        cout << (dist[a][b] == inf ? -1 : dist[a][b]) << '\n';
+    }
+    return 0;
+}
+
+// TLE
+int dijsktra() {
     cin.tie(nullptr)->sync_with_stdio(false);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
