48903a5b368d76dbc98b70006f0b5d2e84964cd2
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-18 11:03:51 +0530

    Restrucutred

---
+++ CodeForces/script.py
@@ -0,0 +1,52 @@
+import os
+mypath = 'D:/NITH/CP/CodeForces/'
+
+def getContestName(problem:str):
+    contest_name = ""
+    for c in problem:
+        if not c.isdigit():
+            break
+        contest_name += c
+    return contest_name
+
+def makeFolders():
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) and f[0].isdigit()]
+    # print(onlyfiles)
+    # print(len(onlyfiles))
+    # mkdir('D:/NITH/CP/CodeForces/Test')
+    # print(os.path.exists(mypath))
+
+    # def manageFiles():
+    #     try:
+    #         os.mkdir(os.path.join(mypath,'Test'))
+    #         print('Created')
+    #     except:
+    #         print('Failed')
+
+    contests={}
+    added = []
+
+    #  make a dict {contest :[problems]}
+    for code in onlyfiles:
+        contest_name = getContestName(code)
+        if contest_name not in added:
+            temp =[]
+            temp.append(code)
+            contests[contest_name] = temp
+            added.append(contest_name)
+        else:
+            contests[contest_name].append(code)
+
+    print(contests)
+
+    # make the folders and move the files
+    for contest in contests:
+        try:
+            os.mkdir(os.path.join(mypath,contest))
+            for code in contests[contest]:
+                os.rename('D:/NITH/CP/CodeForces/'+code, 'D:/NITH/CP/CodeForces/'+contest+'/'+code)
+        except:
+            print(contest,'Already exists!')
+
+
+makeFolders()
