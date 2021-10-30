b81eb4bfab65131a412f5a4330a0b67a863d60ca
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-30 19:04:19 +0530

    Added ABC225

--- AtCoder/ABC225/D.cpp
+++ AtCoder/ABC225/D.cpp
@@ -1,42 +1,29 @@
-#ifdef LOCAL
-// https://github.com/AkshatAggarwal14/Competetive-Programming
-#include "Akshat.hpp"
-#else
 #include "bits/stdc++.h"
 using namespace std;
-#define dbg(...)
-#endif
-using ll = int64_t;
-auto sz = [](const auto &container) -> ll { return container.size(); };
+auto sz = [](const auto &container) -> int { return int(container.size()); };
 
 void Solution() {
-    ll n, q, type, x, y;
+    int n, q, type, x, y;
     cin >> n >> q;
-    vector<ll> parent(n + 1, -1), child(n + 1, -1), temp;
-    for (ll i = 1; i <= n; ++i) parent[i] = i;
+    vector<int> parent(n + 1, -1), child(n + 1, -1), temp;
+    for (int i = 1; i <= n; ++i) parent[i] = i;
     while (q--) {
         cin >> type;
         if (type == 3) {
             cin >> x;
             temp.clear();
-            while (x != parent[x]) x = parent[x];
-            while (child[x] != -1) {
-                temp.push_back(x);
-                x = child[x];
-            }
+            while (x != parent[x]) x = parent[x];  // go to the first car
+            while (child[x] != -1) temp.push_back(x), x = child[x];
             temp.push_back(x);
             cout << sz(temp) << ' ';
-            for (ll &ele : temp) cout << ele << ' ';
+            for (int &ele : temp) cout << ele << ' ';
             cout << '\n';
         } else {
             cin >> x >> y;
-            if (type == 1) {
-                parent[y] = x;
-                child[x] = y;
-            } else {
-                parent[y] = y;
-                child[x] = -1;
-            }
+            if (type == 1)
+                parent[y] = x, child[x] = y;
+            else
+                parent[y] = y, child[x] = -1;
         }
     }
 }
@@ -48,9 +35,6 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cout << fixed << setprecision(12);
-    // ll tc; cin >> tc; while (tc--)
     Solution();
-    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
