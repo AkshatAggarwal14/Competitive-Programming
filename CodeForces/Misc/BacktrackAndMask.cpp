3e231a0da1ed63049b93ea710fd38162571981a3
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-11-05 12:24:05 +0530

    Restructured

--- CodeForces/Misc/BacktrackAndMask.cpp
+++ CodeForces/Misc/BacktrackAndMask.cpp
@@ -1,13 +1,5 @@
-#ifdef LOCAL
-// https://github.com/AkshatAggarwal14/Competetive-Programming
 #include "Akshat.hpp"
-#else
-#include "bits/stdc++.h"
-using namespace std;
-#define dbg(...)
-#endif
 using ll = int64_t;
-auto sz = [](const auto &container) -> ll { return container.size(); };
 
 vector<vector<int>> ans;
 void backtrack(vector<int> a, int sum) {
@@ -46,25 +38,30 @@ void backtrack() {
 void masking() {
     ll n;
     cin >> n;
+    set<vector<ll>> res;
     for (ll mask = 0; mask < (1LL << n); mask++) {
-        ll sum = 0;
+        ll cnt = 0;
+        vector<ll> temp;
         for (ll i = 0; i < n; i++) {
             if (mask & (1LL << i)) {
+                if (cnt) temp.push_back(cnt);
+                cnt = 0;
             }
+            cnt++;
         }
+        if (cnt) temp.push_back(cnt);
+        res.insert(temp);
     }
+    dbg(res);
 }
 
 // clang-format off
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
+#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cout << fixed << setprecision(12);
-    // ll tc; cin >> tc; while (tc--)
     backtrack();
-    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
