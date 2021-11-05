c0abacbc063e3f402d1be18b3d9844c2f609801c
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-11-05 10:10:52 +0530

    Restructured

--- CodeForces/del.py
+++ CodeForces/del.py
@@ -4,17 +4,16 @@ import sys
 mypath = 'D:/NITH/CP/CodeForces/'
 
 
-def main(files: list):
-    folders = [f for f in os.listdir(mypath) if len(f) >= 4 and f[:-4] in need]
-    print(folders)
-    # os.rmdir(mypath)
-    pass
+def main():
+    need = ['.'+_ for _ in sys.argv[1:]]
+    if(len(need) == 0):
+        return
+    files = [f for f in os.listdir(mypath) if len(f) >= 4 and f[-4:] in need]
+    print(files)
+    for file in files:
+        # print(os.path.join(mypath, file))
+        os.remove(os.path.join(mypath, file))
 
 
 if __name__ == "__main__":
-    need = ['.'+_ for _ in sys.argv[1:]]
-    main(need)
-    # getFiles()
-    os.system('git add -A')
-    os.system('git commit -m "Restructured"')
-    os.system('git push')
+    main()
