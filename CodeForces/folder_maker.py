5da6be00d71ff30c0ef7a5c62399330c821dd059
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-11-05 10:01:28 +0530

    Restructured

--- CodeForces/folder_maker.py
+++ CodeForces/folder_maker.py
@@ -2,7 +2,8 @@
 import os
 mypath = 'D:/NITH/CP/CodeForces/'
 
-def getContestName(problem:str):
+
+def getContestName(problem: str):
     contest_name = ""
     for c in problem:
         if not c.isdigit():
@@ -10,17 +11,19 @@ def getContestName(problem:str):
         contest_name += c
     return contest_name
 
-def makeFolders():
-    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) and f[0].isdigit()]
 
-    contests={}
+def makeFolders():
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(
+        os.path.join(mypath, f)) and f[0].isdigit()]
+
+    contests = {}
     added = []
 
     #  make a dict {contest :[problems]}
     for code in onlyfiles:
         contest_name = getContestName(code)
         if contest_name not in added:
-            temp =[]
+            temp = []
             temp.append(code)
             contests[contest_name] = temp
             added.append(contest_name)
@@ -32,29 +35,35 @@ def makeFolders():
     # make the folders and move the files
     for contest in contests:
         try:
-            os.mkdir(os.path.join(mypath,contest))
+            os.mkdir(os.path.join(mypath, contest))
             for code in contests[contest]:
-                os.rename('D:/NITH/CP/CodeForces/'+code, 'D:/NITH/CP/CodeForces/'+contest+'/'+code)
+                os.rename('D:/NITH/CP/CodeForces/'+code,
+                          'D:/NITH/CP/CodeForces/'+contest+'/'+code)
         except:
-            print(contest,'Already exists!')
+            print(contest, 'Already exists!')
             for code in contests[contest]:
                 try:
-                    os.rename('D:/NITH/CP/CodeForces/'+code, 'D:/NITH/CP/CodeForces/'+contest+'/'+code)
+                    os.rename('D:/NITH/CP/CodeForces/'+code,
+                              'D:/NITH/CP/CodeForces/'+contest+'/'+code)
                 except:
-                    print(contest,'/',code,'Already exists!')
+                    print(contest, '/', code, 'Already exists!')
+
 
 def getFiles():
-    folders = [f for f in os.listdir(mypath) if os.path.isdir(os.path.join(mypath, f)) and f[0].isdigit()]
+    folders = [f for f in os.listdir(mypath) if os.path.isdir(
+        os.path.join(mypath, f)) and f[0].isdigit()]
     # print(folders)
     for contest in folders:
-        files = [f for f in os.listdir(mypath+contest) if os.path.isfile(os.path.join(mypath+contest, f)) and f[0].isdigit()]
+        files = [f for f in os.listdir(
+            mypath+contest) if os.path.isfile(os.path.join(mypath+contest, f)) and f[0].isdigit()]
         # print(files)
         for code in files:
             # print(contest+'/'+code)
             os.rename(mypath+contest+'/'+code, mypath+code)
         os.rmdir(mypath+contest)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     makeFolders()
     # getFiles()
     os.system('git add -A')
