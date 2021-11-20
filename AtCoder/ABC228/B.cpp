63dcbcf9de3e029e99b5adceafb9d1a425c8c2a2
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-11-20 23:53:41 +0530

    Upsolved ABC228

---
+++ AtCoder/ABC228/B.cpp
@@ -0,0 +1,43 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
+
+void Solution() {
+    ll n, X, ans = 0;
+    cin >> n >> X;
+    X--;
+    vector<ll> a(n);
+    for (auto &x : a) cin >> x, --x;
+    while (a[X] != -1) {
+        ans++;
+        ll t = X;
+        X = a[X];
+        a[t] = -1;
+    }
+    cout << ans << '\n';
+}
+
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
+    return 0;
+}
