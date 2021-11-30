73b42262330c0d2cc55b419f4c682926215d5cec
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-11-30 17:34:54 +0530

    Upsolved

---
+++ CodeForces/1333/1333A.cpp
@@ -0,0 +1,36 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
+
+void Solution() {
+    ll n, m;
+    cin >> n >> m;
+    vector<string> res(n, string(m, 'B'));
+    res[0][0] = 'W';
+    for (auto &x : res) cout << x << '\n';
+}
+
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    ll tc; cin >> tc; while (tc--)
+    Solution();
+    return 0;
+}
