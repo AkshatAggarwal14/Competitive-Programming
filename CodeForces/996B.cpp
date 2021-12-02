8cf7a75afa90164bf2a94b8c9cce5281159b0517
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-02 13:58:07 +0530

    Upsolved + BS

---
+++ CodeForces/996B.cpp
@@ -0,0 +1,60 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
+
+template <bool b>
+auto binsearch(auto l, auto r, const auto &pred) {
+    --l, ++r;
+    for (decltype(l) m; m = midpoint(l, r), r > l + 1;) (pred(m) ? l : r) = m;
+    return (b ? l : r);
+}
+
+// returns first i in [l, r], p(i) false, and if none found, returns r + 1
+auto find_first_false(auto l, auto r, const auto &p) {
+    return binsearch<false>(l, r, p);
+}
+
+// returns last i in [l, r], p(i) true, and if none found, returns l - 1
+auto find_last_true(auto l, auto r, const auto &p) {
+    return binsearch<true>(l, r, p);
+}
+
+void Solution() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    // we visit gate i at times i + tn
+    // i + tn > a_i
+    // bin search
+    for (ll i = 0; ll & x : a) {
+        cin >> x;
+        b[i] = find_first_false(0LL, (ll)1e9, [&](ll t) { return (i + 1) + t * n <= a[i]; });
+        ++i;
+    }
+    cout << distance(b.begin(), min_element(all(b))) + 1 << '\n';
+}
+
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
+    return 0;
+}
