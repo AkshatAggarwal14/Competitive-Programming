b016754b68a65862acff7922a6c3fb72ecd9c74f
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-03 07:01:02 +0530

    -

--- Akshat.hpp
+++ Akshat.hpp
@@ -1,53 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// namespace Color {
-// enum Code {
-//     FG_RED = 31,
-//     FG_GREEN = 32,
-//     FG_YELLOW = 33,
-//     FG_BLUE = 34,
-//     FG_DEFAULT = 39
-// };
-// class ColorDebugger {
-//     Code code;
-
-//    public:
-//     ColorDebugger(Code pCode) : code(pCode) {}
-//     template <class T>
-//     ColorDebugger& operator<<(const T& other) {
-//         cerr << "\033[" << code << "m" << other << "\033[" << FG_DEFAULT << "m";
-//         return *this;
-//     }
-//     ColorDebugger& operator<<(ostream& (*pManip)(ostream&)) {
-//         (*pManip)(cerr);
-//         return *this;
-//     }
-// };
-// }  // namespace Color
-
-// using namespace Color;
-// ColorDebugger ycerr(FG_YELLOW);
-// ColorDebugger rcerr(FG_RED);
-// ColorDebugger bcerr(FG_BLUE);
-// ColorDebugger gcerr(FG_GREEN);
-
-string to_string(string s) {
-    return '"' + s + '"';
-}
-string to_string(char ch) {
-    string s(1, ch);
-    return '\'' + s + '\'';
-}
-string to_string(const char* s) {
-    return to_string((string)s);
-}
-string to_string(bool b) {
-    return (b ? "true" : "false");
-}
+// forward declarations
 template <typename A, typename B>
-string to_string(pair<A, B> p) {
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+string to_string(pair<A, B> p);
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+// basic
+string to_string(const string& s) { return '"' + s + '"'; }
+string to_string(const char* s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+
+// others
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
 }
 template <typename A>
 string to_string(A v) {
@@ -62,6 +49,18 @@ string to_string(A v) {
     }
     res += "}";
     return res;
+}
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")"; }
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")"; }
+
+string to_string(char ch) {
+    string s(1, ch);
+    return '\'' + s + '\'';
 }
 
 #define beauty(...) pretty_print(#__VA_ARGS__, __VA_ARGS__)
@@ -88,8 +87,9 @@ void pretty_print(const char* names, Arg1&& arg1, Args&&... args) {
          << "\033[39m";                 \
     beauty(__VA_ARGS__)
 
-#define debug(...) cerr << "Line " << __LINE__ << ": ";\
-                   debug_out(#__VA_ARGS__, __VA_ARGS__)
+#define debug(...)                       \
+    cerr << "Line " << __LINE__ << ": "; \
+    debug_out(#__VA_ARGS__, __VA_ARGS__)
 
 // base case for template recursion when one argument remains
 template <typename Arg1>
