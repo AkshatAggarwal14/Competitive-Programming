647f6c17fa4a4ed6498f80ed72151a31880a2f15
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-15 10:42:15 +0530

    Upsolved

--- CodeForces/1618/1618D.cpp
+++ CodeForces/1618/1618D.cpp
@@ -16,14 +16,14 @@ bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 
 void Solution() {
     int n, k;
-    ll res = 0, sum = 0;
+    ll sum = 0;
     cin >> n >> k;
     vector<ll> arr(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
     sort(arr.rbegin(), arr.rend());
-    for (int i = 0; i < k; i++) res += arr[i + k] / arr[i];  //! greatest/(k+1th greatest)
-    for (int i = 2 * k; i < n; i++) res += arr[i];           // least k added as it is
-    cout << res << '\n';
+    for (int i = 0; i < k; i++) sum += arr[i + k] / arr[i];  //! greatest/(k+1th greatest)
+    for (int i = 2 * k; i < n; i++) sum += arr[i];           // least k added as it is
+    cout << sum << '\n';
 }
 
 // clang-format off
