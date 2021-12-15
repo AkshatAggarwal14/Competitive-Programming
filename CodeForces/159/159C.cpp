1f3bdef713b4d82c0a8d531cb2082b59d84bb734
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-15 10:18:13 +0530

    Upsolved

---
+++ CodeForces/159/159C.cpp
@@ -0,0 +1,63 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+template <class key, class value, class cmp = std::less<key>>
+using o_map = __gnu_pbds::tree<key, value, cmp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+void Solution() {
+    ll k, q, i;
+    string s1, s = "";
+    char c;
+    cin >> k >> s1;
+    for (i = 0; i < k; ++i) s += s1;  //! :D
+    k = sz(s);
+    vector<o_set<ll>> idx(26);
+    vector<bool> mark(k, true);
+    for (i = 0; char &x : s) idx[x - 'a'].insert(i++);
+    cin >> q;
+    while (q--) {
+        cin >> i >> c;
+        ll id = *idx[c - 'a'].find_by_order(i - 1);
+        mark[id] = false;
+        idx[c - 'a'].erase(id);
+    }
+    for (i = 0; i < k; ++i)
+        if (mark[i]) cout << s[i];
+    cout << '\n';
+}
+
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
+    return 0;
+}
