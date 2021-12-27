f1c4ead8c0c3d3ab6acf8cef2f76c84c50e13e49
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-27 18:45:34 +0530

    Added Tree problems

---
+++ CodeForces/522/522A.cpp
@@ -0,0 +1,48 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    map<string, ll> chain;
+    chain["polycarp"] = 1;
+    ll n, ans = 1;
+    cin >> n;
+    string a, b, c;
+    while (n--) {
+        cin >> a >> c >> b;
+        transform(all(a), a.begin(), ::tolower);
+        transform(all(b), b.begin(), ::tolower);
+        chain[a] = chain[b] + 1;
+        amax(ans, chain[a]);
+    }
+    dbg(chain);
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
