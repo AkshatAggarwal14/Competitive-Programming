7e05e529fee6e11feb16f8fb1268ea2e309bfee1
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-28 11:48:50 +0530

    Added and Upsolved R1622

--- CodeForces/1622/1622D.cpp
+++ CodeForces/1622/1622D.cpp
@@ -16,14 +16,11 @@ constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true
 
 const ll N = 5000;
 const ll MOD = 998244353;
-inline ll M(const ll &x) { return ((x % MOD) + MOD) % MOD; }
-inline ll add(const ll &x, const ll &y) { return (M(x) + M(y)) % MOD; }
-inline ll mul(const ll &x, const ll &y) { return (M(x) * M(y)) % MOD; }
-
+ll M(ll x) { return ((x % MOD) + MOD) % MOD; }
+ll add(ll x, ll y) { return (M(x) + M(y)) % MOD; }
+ll mul(ll x, ll y) { return (M(x) * M(y)) % MOD; }
 array<ll, N + 1> fact;
 array<ll, N + 1> inv;
-array<ll, N + 1> invf;
-
 ll modpow(ll x, ll y, ll m) {
     if (y == 0) return 1 % m;
     ll u = modpow(x, y / 2, m);
@@ -31,24 +28,29 @@ ll modpow(ll x, ll y, ll m) {
     if (y % 2 == 1) u = (u * x) % m;
     return u;
 }
-
 // a**(p - 1) % p == 1
 // a*(a**(p - 2)) % p == 1
 // a**(p - 2) % p == 1 / a
 ll inverse(ll a, ll m = MOD) { return modpow(a, m - 2, m); }  //Fermats little theorem
-
 void init() {
+    fact[0] = 1;
+    for (ll i = 1; i <= N; i++) fact[i] = (fact[i - 1] * i) % MOD;
+    inv[N] = inverse(fact[N]);
+    for (ll i = N - 1; i >= 0; --i) inv[i] = mul(inv[i + 1], (i + 1));
+
+    /*
     fact[0] = inv[0] = invf[0] = fact[1] = inv[1] = invf[1] = 1;
     for (ll i = 2; i <= N; i++) {
         fact[i] = mul(fact[i - 1], i);
         inv[i] = M(-mul((MOD / i), inv[MOD % i]));
         invf[i] = mul(invf[i - 1], inv[i]);
     }
+    */
 }
 
-ll C(const ll &a, const ll &b) {
+ll C(ll a, ll b) {
     if (a < b) return 0;
-    return mul(mul(fact[a], invf[b]), invf[a - b]);
+    return mul(mul(fact[a], inv[b]), inv[a - b]);
 }
 
 void Solution() {
