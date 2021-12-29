bc8be1f328466ece65a1cb8a0b66adde14ace095
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-29 14:42:11 +0530

    Upsolved

--- CodeForces/del.py
+++ CodeForces/del.py
@@ -7,13 +7,14 @@ mypath = os.getcwd()
 def fileUTIL(filename: str):
     if '.' not in filename:
         return None
-    return '.'+filename.split('.')[-1]
+    return '.' + filename.split('.')[-1]
 
 
 def deleter():
     if '--help' in sys.argv or '-h' in sys.argv:
         print('Usage:\npython .\\del.py <extensions> (- for no extension)]')
         return
+
     need = ['.' + _ for _ in sys.argv[1:]]
     files = [f for f in os.listdir(mypath) if os.path.isfile(
         os.path.join(mypath, f)) and fileUTIL(f) in need]
