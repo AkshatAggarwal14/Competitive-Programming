94b4ce527a7b90825d187bdc896e54ce566e0f37
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-31 18:39:51 +0530

    Upsolved

---
+++ AtCoder/ABC208/A.cpp
@@ -0,0 +1,37 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    ll a, b;
+    cin >> a >> b;
+    cout << ((b >= a && b <= 6 * a) ? "Yes\n" : "No\n");
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
---
+++ AtCoder/ABC208/B.cpp
@@ -0,0 +1,47 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    ll p;
+    cin >> p;
+    vector<ll> fact(10);
+    fact[0] = 1;
+    for (ll i = 1; i < 10; ++i) fact[i] = fact[i - 1] * (i + 1);
+    ll coins = 0;
+    // only valid as successive coin values are multiple of previous
+    for (ll i = 9; i >= 0; --i) {
+        ll times = p / fact[i];
+        coins += times;
+        p -= times * fact[i];
+    }
+    cout << coins << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
