6173c68eed914db86e0ff5caa646bd3378a376c0
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-07 15:11:57 +0530

    Upsolved

---
+++ CodeForces/12/12B.cpp
@@ -0,0 +1,39 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    string n, m;
+    cin >> n >> m;
+    sort(all(n));
+    while (n[0] == '0' && n != "0") next_permutation(all(n));
+    cout << ((n == m) ? "OK" : "WRONG_ANSWER");
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
