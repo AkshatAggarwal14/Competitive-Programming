89e2d248ccc4fddeba51afd7939e0d415bf3808b
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-12 23:10:37 +0530

    Added EXUN2021

---
+++ CodeChef/SUMPARITY.cpp
@@ -0,0 +1,44 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    ll N, A;
+    cin >> N >> A;
+    ll bit = (A & 1);
+    if (N == 1) {
+        cout << ((bit) ? "Odd\n" : "Even\n");
+    } else if (N & 1) {
+        cout << ((bit) ? "Odd\n" : "Impossible\n");
+    } else {
+        cout << ((bit) ? "Even\n" : "Impossible\n");
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
