121ceedf5ab5f1986a68906d33c1125b9171984c
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-15 23:49:03 +0530

    Upsolved ABC235

--- AtCoder/ABC235/C.cpp
+++ AtCoder/ABC235/C.cpp
@@ -15,32 +15,19 @@ template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T>
-using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T>
-using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//member functions :
-//1. order_of_key(k) : number of elements strictly lesser than k
-//2. find_by_order(k) : k-th element in the set
-template <class key, class value, class cmp = std::less<key>>
-using o_map = __gnu_pbds::tree<key, value, cmp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-
 void Solution() {
     ll n, q, x, k;
     cin >> n >> q;
     vector<ll> a(n);
-    map<ll, o_set<ll>> mp;
-    for (ll i = 0; i < n; ++i) cin >> a[i], mp[a[i]].insert(i);
+    map<ll, vector<ll>> mp;
+    for (ll i = 0; i < n; ++i) cin >> a[i], mp[a[i]].push_back(i);
     while (q--) {
         cin >> x >> k, --k;
         // kth occurence of x
         if (!mp.count(x) || sz(mp[x]) <= k)
             cout << "-1\n";
         else
-            cout << *mp[x].find_by_order(k) + 1 << '\n';
+            cout << mp[x][k] + 1 << '\n';
     }
 }
 
