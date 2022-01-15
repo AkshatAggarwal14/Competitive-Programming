f345e5637120792d07732c338112aee420d6b96d
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-15 23:44:41 +0530

    Upsolved

--- CodeForces/1627/1627C.cpp
+++ CodeForces/1627/1627C.cpp
@@ -19,43 +19,41 @@ void Solution() {
     ll n;
     cin >> n;
     vector<vector<ll>> g(n);
-    vector<ll> deg(n, 0);
-    vector<array<ll, 2>> edges(n - 1);
-    map<pair<ll, ll>, ll> color;
+    vector<pair<ll, ll>> edges(n - 1);
+    map<pair<ll, ll>, ll> edge_color;
     for (ll i = 0; i < n - 1; ++i) {
         ll u, v;
         cin >> u >> v;
         u--, v--;
-        edges[i] = {u, v};
+        edges[i] = minmax(u, v);
         g[u].push_back(v), g[v].push_back(u);
-        ++deg[v], ++deg[u];
     }
-    if (*max_element(all(deg)) > 2) return void(cout << "-1\n");
-    // always in the form of a line
-    vector<ll> ans(n - 1);
+
+    ll leaf = -1;  // always in the form of a line
+    for (ll i = 0; i < n; i++) {
+        if (sz(g[i]) > 2) return void(cout << -1 << '\n');
+        if (sz(g[i]) == 1) leaf = i;
+    }
     queue<ll> bfs;
     vector<bool> vis(n, false);
-    ll start = find(all(deg), 1) - deg.begin();  // start with degree 1
-    bfs.push(start);
-    vis[start] = true;
+    vis[leaf] = true;
+    bfs.push(leaf);
     ll col = 0;
     while (!bfs.empty()) {
         ll parent = bfs.front();
         bfs.pop();
         for (ll &child : g[parent]) {
             if (vis[child]) continue;
-            color[{parent, child}] = col;
+            edge_color[minmax(parent, child)] = col;
             col ^= 1;
             bfs.push(child);
             vis[child] = true;
         }
     }
-    for (ll i = 0; i < n - 1; ++i) {
-        ll u = edges[i][0], v = edges[i][1];
-        if (!color.count({u, v})) swap(u, v);
-        ans[i] = color[{u, v}] + 2;  // 2 and 3, sum = 5
-    }
-    for (ll &A : ans) cout << A << ' ';
+
+    // 2 and 3, sum = 5
+    for (ll i = 0; i < n - 1; ++i)
+        cout << edge_color[edges[i]] + 2 << ' ';
     cout << '\n';
 }
 
