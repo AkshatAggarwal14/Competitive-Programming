76fa889a759ecc6ec18510d7b709ed75c78571c5
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-15 19:57:36 +0530

    Added ABC235

---
+++ AtCoder/ABC235/A.cpp
@@ -0,0 +1,38 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    char a, b, c;
+    cin >> a >> b >> c;
+    ll A = a - '0', B = b - '0', C = c - '0';
+    cout << 111 * (A + B + C) << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
---
+++ AtCoder/ABC235/D.cpp
@@ -0,0 +1,82 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void BFS() {
+    map<ll, ll> operations;
+    ll a, n;
+    cin >> a >> n;
+    queue<ll> bfs;
+    bfs.push(1);
+    operations[1] = 0;
+    while (!bfs.empty()) {
+        ll node = bfs.front();
+        bfs.pop();
+        ll n1 = node * a;
+        if (!operations.count(n1) && sz(to_string(n1)) <= sz(to_string(n)))
+            bfs.push(n1), operations[n1] = operations[node] + 1;
+        string S = to_string(node);
+        if (S.back() != '0' && sz(S) >= 2) {
+            string S_ = "";
+            S_ += S.back();
+            S_ += S.substr(0, sz(S) - 1);
+            ll n2 = stoll(S_);
+            if (!operations.count(n2) && sz(to_string(n2)) <= sz(to_string(n)))
+                bfs.push(n2), operations[n2] = operations[node] + 1;
+        }
+    }
+    if (!operations.count(n)) return void(cout << "-1\n");
+    cout << operations[n] << '\n';
+}
+
+void DFS() {
+    map<ll, ll> operations;
+    ll a, n;
+    cin >> a >> n;
+    auto dfs = [&](const auto &self, ll N, ll ops) {
+        if (operations.count(N)) return;
+        operations[N] = ops;
+        ll n1 = N * a;
+        if (sz(to_string(n1)) <= sz(to_string(n))) self(self, n1, ops + 1);
+        string S = to_string(N);
+        if (S.back() != '0' && sz(S) >= 2 && sz(S) <= sz(to_string(n))) {
+            string S_ = "";
+            S_ += S.back();
+            S_ += S.substr(0, sz(S) - 1);
+            ll n2 = stoll(S_);
+            self(self, n2, ops + 1);
+        }
+    };
+    dfs(dfs, 1, 0);
+    if (!operations.count(n)) return void(cout << "-1\n");
+    cout << operations[n] << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        DFS();
+        // BFS();
+    }
+    return 0;
+}
