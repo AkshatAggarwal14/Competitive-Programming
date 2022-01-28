b301f2eaffa4007544e96053ba61f883866f3ed5
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-28 08:47:29 +0530

    Added R1631

--- CodeForces/1631/1631B.cpp
+++ CodeForces/1631/1631B.cpp
@@ -1,26 +1,14 @@
-#ifdef LOCAL
-// https://github.com/AkshatAggarwal14/Competetive-Programming
-#include "Akshat.hpp"
-#else
 #include "bits/stdc++.h"
 using namespace std;
-#define dbg(...)
-#endif
 using ll = long long;
 constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
-#define all(x) (x).begin(), (x).end()
-template <class T, class U = T>
-constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
-template <class T, class U = T>
-constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
-const ll MOD = 1e9 + 7;
 
 void Solution() {
     ll n;
     cin >> n;
     vector<ll> a(n);
     for (ll &A : a) cin >> A;
-    ll last = a.back();
+    ll last = a[n - 1];
     auto cnt_from_back = [&]() -> ll {
         ll cnt = 0, pos = n - 1;
         while (pos >= 0 && a[pos] == last) ++cnt, --pos;
@@ -38,11 +26,6 @@ void Solution() {
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    cout << fixed << setprecision(12);
     int tc = 1;
     cin >> tc;
     while (tc--) {
