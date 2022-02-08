8a9861f4ab3e814781b7f66a4df0dfe76184b9ba
AkshatAggarwal14 <akshataggarwal1411@gmail.com>
2022-02-08 13:11:47 +0530

    Upsolved

--- CodeChef/REMADJ.cpp
+++ CodeChef/REMADJ.cpp
@@ -14,111 +14,37 @@ constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true
 template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
+
 void Solution() {
     ll n;
     cin >> n;
     vector<ll> a(n);
     for (auto &i : a) cin >> i;
-    ll SUM = accumulate(all(a), 0LL);
-    if (SUM < 0) {
-        SUM = -SUM;
-        for (ll &i : a) i = -i;
-    }
+    ll sum = accumulate(all(a), 0LL);
     // sum of array remains constant
     // calculate minimum number of operations required to make all elements of array equal if in one operation we can replace two adjacent elements with there sum
     // so, each element must be sum/x for some divisor x.
-    vector<ll> choices;
-    if (SUM == 0) {
-        choices.push_back(0);
-    } else {
-        for (ll i = 1; i * i <= SUM; i++) {
-            if (SUM % i == 0) {
-                choices.push_back(i);
-                if (i != SUM / i) choices.push_back(SUM / i);
+    // the original array A can be partitioned into subarrays [1…P1],[P1+1,…P2],…,[Py−2+1,…,Py−1],[Py−1+1,N]
+    ll ans = n - 1;
+    for (ll i = 1; i <= n; i++) {
+        if (abs(sum) % i == 0) {
+            ll ele = i, val = sum / i;
+            int sum = 0;
+            for (ll &x : a) {
+                sum += x;
+                if (sum == val) sum = 0, ele--;
             }
+            if (ele <= 0) amin(ans, n - i);  //! whyyy
         }
     }
-    ll best = n - 1;  // atmost
-    for (auto &potential_ans : choices) {
-        for (ll _ = 0; _ < 2; ++_) {
-            vector<pair<ll, ll>> Rs;
-            ll last = -1, sum = -1, ans = 0;
-            for (ll i = 0; i < n;) {
-                last = i;
-                sum = a[i];
-                ++i;
-                while (i < n && sum != potential_ans) {
-                    sum += a[i];
-                    i++;
-                }
-                if (_ == 0) {
-                    while (i < n && a[i] == 0) {  //! fuck zeroes
-                        i++;
-                    }
-                }
-                Rs.push_back({last, i - 1});
-                ans += (i - 1) - last;
-            }
-            // validate
-            bool valid = true;
-            for (auto &[x, y] : Rs) {
-                ll S = 0;
-                for (ll i = x; i <= y; ++i) {
-                    S += a[i];
-                }
-                if (S != potential_ans) valid = false;
-            }
-            // check only if validated
-            if (valid && !Rs.empty() && Rs.back().second == n - 1)
-                amin(best, ans);
-            if (best == 0) break;
-        }
-    }
-    reverse(all(a));
-    for (auto &potential_ans : choices) {
-        for (ll _ = 0; _ < 2; ++_) {
-            vector<pair<ll, ll>> Rs;
-            ll last = -1, sum = -1, ans = 0;
-            for (ll i = 0; i < n;) {
-                last = i;
-                sum = a[i];
-                ++i;
-                while (i < n && sum != potential_ans) {
-                    sum += a[i];
-                    i++;
-                }
-                if (_ == 0) {
-                    while (i < n && a[i] == 0) {  //! fuck zeroes
-                        i++;
-                    }
-                }
-                Rs.push_back({last, i - 1});
-                ans += (i - 1) - last;
-            }
-            // validate
-            bool valid = true;
-            for (auto &[x, y] : Rs) {
-                ll S = 0;
-                for (ll i = x; i <= y; ++i) {
-                    S += a[i];
-                }
-                if (S != potential_ans) valid = false;
-            }
-            // check only if validated
-            if (valid && !Rs.empty() && Rs.back().second == n - 1)
-                amin(best, ans);
-            if (best == 0) break;
-        }
-    }
-    assert(best >= 0 && best <= n - 1);
-    cout << best << '\n';
+    cout << ans << '\n';
 }
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 #ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
+    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
 #endif
     cout << fixed << setprecision(12);
     int tc = 1;
