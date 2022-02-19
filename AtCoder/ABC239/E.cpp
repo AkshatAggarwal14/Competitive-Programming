1cec9aea1213254cac9ef3ad62c0a8aed1640ddf
AkshatAggarwal14 <akshataggarwal1411@gmail.com>
2022-02-19 21:06:43 +0530

    Upsolved

--- AtCoder/ABC239/E.cpp
+++ AtCoder/ABC239/E.cpp
@@ -16,43 +16,41 @@ void Solution() {
     cin >> n >> q;
     vector<ll> x(n);
     for (ll &X : x) cin >> X;
-    vector<vector<ll>> g(n);
+    vector<vector<ll>> g(n), ans(n);
     for (ll i = 0, u, v; i < n - 1; ++i) {
         cin >> u >> v, --u, --v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    vector<multiset<ll>> ans(n);  // 20 atmax in each
 
-    auto add = [&](ll index, ll value) {
-        if (sz(ans[index]) < 20) {
-            ans[index].insert(value);
+    auto put = [&](ll value, ll in_index) {
+        if (sz(ans[in_index]) < 20) {
+            ans[in_index].push_back(value);
         } else {
-            auto it = ans[index].begin();
-            if (value > *it) {
-                ans[index].erase(it);
-                ans[index].insert(value);
+            ll mini = *min_element(all(ans[in_index]));
+            if (mini < value) {
+                ans[in_index].erase(find(all(ans[in_index]), mini));
+                ans[in_index].push_back(value);
             }
         }
     };
 
     // add children's ans to parents + add parent itself
     auto dfs = [&](const auto &self, ll node, ll parent = -1) -> void {
-        add(node, x[node]);
+        put(x[node], node);
         for (ll &child : g[node]) {
             if (child == parent) continue;
             self(self, child, node);
-            for (auto &&res : ans[child]) add(node, res);
+            for (const auto &i : ans[child]) put(i, node);
         }
     };
     dfs(dfs, 0);
 
+    for (ll i = 0; i < n; ++i) sort(all(ans[i]), greater<ll>());
     while (q--) {
         ll v, k;
         cin >> v >> k, --v, --k;
-        auto it = ans[v].begin();
-        for (ll i = 0; i < sz(ans[v]) - 1 - k; ++i) it = next(it);
-        cout << *it << '\n';
+        cout << ans[v][k] << '\n';
     }
 }
 
