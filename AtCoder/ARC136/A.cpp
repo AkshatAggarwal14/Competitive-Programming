071d0263bf7a4c062427c25f5a47a6025b25ffc4
AkshatAggarwal14 <akshataggarwal1411@gmail.com>
2022-02-27 23:03:52 +0530

    Added ARC136

---
+++ AtCoder/ARC136/A.cpp
@@ -0,0 +1,36 @@
+#ifdef LOCAL
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void Solution() {
+    ll n;
+    string s;
+    cin >> n >> s;
+    s = regex_replace(s, regex("A"), "BB");
+    s = regex_replace(s, regex("BB"), "A");
+    cout << s << '\n';
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
+    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
