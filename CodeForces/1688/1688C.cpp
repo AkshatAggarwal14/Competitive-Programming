21b4d375935ca1715a7465ac55c03073577ceae4
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-06-04 00:26:10 +0530

    Upsolved R1688

---
+++ CodeForces/1688/1688C.cpp
@@ -0,0 +1,42 @@
+#ifdef LOCAL
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+auto sz = [](const auto &container) { return int(container.size()); };
+#define all(x) begin(x), end(x)
+
+const ll INF = 1e18;
+const ll N = 1e5 + 5;
+const ll MOD = 1e9 + 7;  // 998244353
+
+//! only starting character occurs odd number of times
+void test() {
+    char ans = 0;
+    ll n;
+    cin >> n;
+    string s;
+    for (ll i = 0; i <= 2 * n; ++i) {
+        cin >> s;
+        for (char &c : s) ans ^= c;
+    }
+    cout << ans << '\n';
+}
+
+int32_t main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
+    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    cin >> tc;
+    for (int tt = 1; tt <= tc; ++tt) {
+        test();
+    }
+    return 0;
+}
