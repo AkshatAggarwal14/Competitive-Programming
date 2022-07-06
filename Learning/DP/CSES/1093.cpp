d9aa261fd517f6500067e40dafa4f46f688b4698
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-07 02:10:41 +0530

    dump

--- Learning/DP/CSES/1093.cpp
+++ Learning/DP/CSES/1093.cpp
@@ -13,19 +13,20 @@ void test() {
     cin >> n;
     sum = n * (n + 1) / 2;
     if (sum & 1) return void(cout << "0\n");
-    vector<vector<int>> dp(n + 1, vector<int>(sum / 2 + 1, 0));
-    // dp[i][j] = ways to get sum j using first i elements i.e. [1..i]
-    for (int i = 0; i <= n; ++i) dp[i][0] = 1;  // sum 0 with first i elements
 
+    vector<vector<int>> dp(2, vector<int>(sum / 2 + 1, 0));
+    // dp[i][j] = ways to get sum j using first i elements i.e. [1..i]
+    // we need only last 2 states
+    dp[0][0] = dp[1][0] = 1;  // sum 0 with 0 elements
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= sum / 2; ++j) {
-            dp[i][j] = dp[i - 1][j];  // subsets with sum j, without ith element
+            dp[i & 1][j] = dp[(i + 1) & 1][j];  // subsets with sum j, without ith element
             if (i <= j)
-                (dp[i][j] += dp[i - 1][j - i]) %= MOD;  // sum j with ith element
+                (dp[i & 1][j] += dp[(i + 1) & 1][j - i]) %= MOD;  // sum j with ith element
         }
     }
     // dp[n - 1][sum / 2] as now we fix 1 element in second subset, so we dont count twice
-    cout << dp[n - 1][sum / 2] << '\n';
+    cout << dp[(n + 1) & 1][sum / 2] << '\n';
 }
 
 int32_t main() {
